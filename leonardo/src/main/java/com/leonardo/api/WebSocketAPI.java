da00bb021a25fbd7798d10495fe53e839d52eab6
Thang Tran <quocthang1100@gmail.com>
2026-01-19 01:53:06 +0700

    This is a combination of 2 commits.
    Update Dockerfile and workflow ci
    
     Please enter the commit message for your changes. Lines starting

--- leonardo/src/main/java/com/leonardo/api/WebSocketAPI.java
+++ leonardo/src/main/java/com/leonardo/api/WebSocketAPI.java
@@ -1,29 +1,29 @@
-package com.leonardo.api;
-
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.stereotype.Controller;
-
-import com.leonardo.entity.ChatMessage;
-
-import java.util.Objects;
-
-@Controller
-public class WebSocketAPI {
-		
- 	@MessageMapping(value = "/sendMessage")
-    @SendTo(value = "/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chat) {
-        return chat;
-    }
-
- 	@MessageMapping(value = "/addUser")
-    @SendTo(value = "/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chat, SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
-        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chat.getSender());
-        return chat;
-    }
-}
+//package com.leonardo.api;
+//
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+//import org.springframework.stereotype.Controller;
+//
+//import com.leonardo.entity.ChatMessage;
+//
+//import java.util.Objects;
+//
+//@Controller
+//public class WebSocketAPI {
+//
+// 	@MessageMapping(value = "/sendMessage")
+//    @SendTo(value = "/topic/public")
+//    public ChatMessage sendMessage(@Payload ChatMessage chat) {
+//        return chat;
+//    }
+//
+// 	@MessageMapping(value = "/addUser")
+//    @SendTo(value = "/topic/public")
+//    public ChatMessage addUser(@Payload ChatMessage chat, SimpMessageHeaderAccessor headerAccessor) {
+//        // Add username in web socket session
+//        Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("username", chat.getSender());
+//        return chat;
+//    }
+//}
